46332e8e6981dfb7444a2b0aa555f5fe5d895e90
Yuttasak Pannawat <yuttasakcom@gmail.com>
2019-03-20 17:19:34 +0700

    create post[author] relation to user

--- src/user/resolvers/Query.js
+++ src/user/resolvers/Query.js
@@ -3,4 +3,17 @@ export default {
     users: async (parent, args, { prisma }, info) =>
       await prisma.query.users(null, info),
   },
+  User: {
+    posts: {
+      resolve: async (parent, args, { prisma }, info) =>
+        await prisma.query.posts({
+          where: {
+            author: {
+              id: parent.id,
+            },
+          },
+          first: 2,
+        }),
+    },
+  },
 };
